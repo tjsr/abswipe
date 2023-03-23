810061e8422284754bf367d0af3ae37408587786
Tim Rowe <tim@tjsr.id.au>
2023-03-24 08:30:08 +1100

    Fix sample

---
+++ src/example.tsx
@@ -0,0 +1,28 @@
+import './examples.css';
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Swiper } from './Swiper';
+import reportWebVitals from './reportWebVitals';
+
+export const doRender = () => {
+  const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+  root.render(
+    <React.StrictMode>
+      <div className="App">
+        <header className="App-header">
+          <p>Swiper sample component.</p>
+          <a className="App-link" href="https://github.com/tjsr/abswipe" target="_blank" rel="noopener noreferrer">
+            https://github.com/tjsr/abswipe
+          </a>
+        </header>
+        <Swiper />
+      </div>
+    </React.StrictMode>
+  );
+
+  // If you want to start measuring performance in your app, pass a function
+  // to log results (for example: reportWebVitals(console.log))
+  // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+  reportWebVitals();
+};
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 export { DualSwiper, SwipableBox } from './components';
 export type { DualSwiperProps, SwipableBoxProps } from './components';
 export type { RGB, RGBA, HEX, Color, SwipeDirection, SelectionAction, SwiperSharedProps } from './types';
+
+import { doRender } from './example';
+doRender();
